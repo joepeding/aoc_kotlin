2e3e2bbb68e0eb1dd7a4222b8a12df12d742b1bb
Joep Eding <joep.eding@nedap.com>
2022-12-02 20:02:21 +0100

    Clean up 2022 D01

--- 2022/d01/d01.kts
+++ 2022/d01/d01.kts
@@ -2,12 +2,22 @@ import java.io.File
 import java.util.SortedSet
 
 val input: String = File("input").readText()
-val inventories: List<List<Long>> = input.split("\n\n").map { it.split("\n").filter { it.isNotEmpty() }.map { it.toLong() } }
+val inventories: List<List<Long>> = input
+    .split("\n\n")
+    .map { inventory ->
+        inventory
+            .split("\n")
+            .filter { snack -> snack.isNotEmpty() }
+            .map { it.toLong() }
+    }
 val inventorySums = inventories.map { it.sum() }
+
 println("answer 1:")
 println(inventorySums.max())
 
 println("answer 2:")
-inventorySums.sortedDescending().also { println(it) }.also { println(it[0] + it[1] + it[2]) }
+inventorySums
+    .sortedDescending()
+    .also { println(it[0] + it[1] + it[2]) }
 
 
