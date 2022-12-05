6fb2772bd9c8b1ae9ea89ca091676a5ac0ccdb57
Joep Eding <joep.eding@nedap.com>
2022-12-05 08:25:29 +0100

    Add 2022 D05

---
+++ 2022/d05/d05.kts
@@ -0,0 +1,46 @@
+import java.io.File
+
+val input: List<String> = File("input").readText().split("\n\n").filter { it.isNotEmpty() }
+val commands: List<Triple<Int, Int, Int>> = input[1]
+    .split("\n")
+    .filter { it.isNotEmpty() }
+    .map { it.drop(5) }
+    .map { it.split(" ") }
+    .map { Triple(it[0].toInt(), it[2].toInt() - 1, it[4].toInt() - 1) }
+
+// Part 1
+print("Answer 1: ")
+var stacks = parseStacksFromInput(input[0])
+commands.forEach { (num, source, dest) ->
+    stacks[dest].addAll(stacks[source].takeLast(num).reversed())
+    (0..(num-1)).forEach { _ -> stacks[source].removeLast() }
+}
+stacks.map { it.lastOrNull() }.let { println(it) }
+
+//Part 2 (parse input again, because original was modified)
+print("Answer 2: ")
+stacks = parseStacksFromInput(input[0])
+commands.forEach { (num, source, dest) ->
+    stacks[dest].addAll(stacks[source].takeLast(num))
+    (0..(num-1)).forEach { _ -> stacks[source].removeLast() }
+}
+stacks.map { it.lastOrNull() }.let { println(it) }
+
+fun parseStacksFromInput(input: String) = input
+    .split("\n")
+    .map { it.drop(1) }
+    .map { it.windowed(1, 4, partialWindows = false) }
+    .dropLast(1)
+    .reversed()
+    .let { lines ->
+        val stacks: MutableList<MutableList<String>> = mutableListOf()
+        (0..8).forEach { _ -> stacks.add(mutableListOf<String>()) }
+        lines.forEach { line ->
+            line.forEachIndexed { i, s ->
+                if (s.isNotBlank()) {
+                    stacks[i].add(s)
+                }
+            }
+        }
+        stacks
+    }
